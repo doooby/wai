03a5c1e7069633a58eff8e4946a7edb2af33ce37
kaplosh <kaplanekt@gmail.com>
2023-03-06 18:45:29 +0100

    integer fn tests

--- lib/wai/mappers/primitives.test.ts
+++ lib/wai/mappers/primitives.test.ts
@@ -1,6 +1,29 @@
 import { wai } from 'wai';
 
+describe('integer function suite', () => {
 
-it('Checks integer output', () => {
-  //const value = wai.integer(4);
-  expect(wai.integer(4)).toEqual(4)})
+  it('Checks integer output, when integer goes in', () => {
+    const value = wai.integer(4);
+    expect(value).toEqual(4)
+  })
+
+  it('Checks error output, when null goes in', () => {
+      expect(() =>
+        wai.integer(null)).toThrow(Error)
+  })
+
+  it('Checks error output, when string goes in', () => {
+    expect(() =>
+      wai.integer('kkk')).toThrow(Error)
+  })
+
+  it('Checks error output, when empty string goes in', () => {
+    expect(() =>
+      wai.integer('')).toThrow(Error)
+  })
+
+  it('Checks error output, when undefined goes in', () => {
+    expect(() =>
+      wai.integer(undefined)).toThrow(Error)
+  })
+})
