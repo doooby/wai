7f6728c3b7c5589996504051146b800ca4fcd0c7
kaplosh <kaplanekt@gmail.com>
2023-03-09 16:59:32 +0100

    primitives tests

--- lib/wai/mappers/primitives.test.ts
+++ lib/wai/mappers/primitives.test.ts
@@ -27,3 +27,47 @@ describe('integer function suite', () => {
       wai.integer(undefined)).toThrow(Error)
   })
 })
+
+describe('string function suite', () => {
+
+  it('Checks string output, when string goes in ', () => {
+    const value = wai.string('kkk');
+    expect(value).toEqual('kkk');
+  })
+
+  it('Check error throw, when integer goes in ', () => {
+    expect(() => wai.string(666)).toThrow(Error)
+  })
+
+  /*it('Check error throw, when empty string goes in', () => {
+    expect(() => wai.string('')).toThrow(Error)
+    // Is empty string valid?
+  })*/
+
+  it('Check error throw, when undefined goes in', () => {
+    expect(() => wai.string(undefined)).toThrow(Error)
+  })
+})
+
+describe('boolean function suite', () => {
+
+  it('Checks boolean output, when boolean goes in', () => {
+    const value_true = wai.boolean(true);
+    const value_false = wai.boolean(false);
+    expect(value_true).toEqual(true)
+    expect(value_false).toEqual(false)
+  })
+
+  it('Checks error throw, when string goes in', () => {
+    expect(() => wai.boolean('true')).toThrow(Error)
+  })
+
+  it('Checks error throw, when undefined goes in', () => {
+    expect(() => wai.boolean(undefined)).toThrow(Error)
+  })
+
+  it('Checks error throw, when empty string goes in', () => {
+    expect(() => wai.boolean('')).toThrow(Error)
+  })
+
+})
